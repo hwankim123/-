db560e5d30d67ff2f7a14a81f8e0bd14766780e8
Hwan Kim <hwankim123@khu.ac.kr>
2021-04-08 15:14:51 +0900

    11727 수정

--- DP/11727 - 2xn 타일링 2.py
+++ DP/11727 - 2xn 타일링 2.py
@@ -1,8 +1,14 @@
 N = int(input())
 
 cache = [0 for i in range(N)]
-cache[0] = 1
-cache[1] = 3
-for i in range(2, N):
-    cache[i] = (cache[i - 2] * 2 + cache[i - 1]) % 10007
-print(cache[N - 1])
+
+if N == 1:
+    print(1)
+elif N == 2:
+    print(3)
+else:
+    cache[0] = 1
+    cache[1] = 3
+    for i in range(2, N):
+        cache[i] = (cache[i - 2] * 2 + cache[i - 1]) % 10007
+    print(cache[N - 1])
