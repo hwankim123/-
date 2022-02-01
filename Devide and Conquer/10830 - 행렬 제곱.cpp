b4e30675c404baa1c78736e155f2c808b4b34f47
Hwan Kim <hwankim123@naver.com>
2022-02-01 11:30:02 +0900

    .

---
+++ Devide and Conquer/10830 - 행렬 제곱.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#define MAXN 5
+
+long long N, B;
+vector<vector<int> > mat;
+vector<vector<int> > mat2;
+vector<vector<int> > result;
+
+vector<vector<int> >cal(vector<vector<int> > m1, vector<vector<int> > m2){
+    vector<vector<int> >result(N, vector<int>(N, 0));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            for(int k = 0; k < N; k++){
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+            result[i][j] %= 1000;
+        }
+    }
+    return result;
+}
+
+void solve(long long b, vector<vector<int> >& cur){
+    if(b == 1){
+        cur = mat;
+        return;
+    }
+    if(b == 2){
+        cur = mat2;
+        return;
+    }
+    vector<vector<int> >temp1(N, vector<int>(N, 0));
+    vector<vector<int> >temp2(N, vector<int>(N, 0));
+    solve(b / 2, temp1);
+    if(b % 2 == 0){
+        cur = cal(temp1, temp1);
+    }
+    else{
+        temp2 = cal(temp1, mat);
+        cur = cal(temp1, temp2);
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> B;
+    mat = vector<vector<int> >(N, vector<int>(N, 0));
+    mat2 = vector<vector<int> >(N, vector<int>(N, 0));
+    result = vector<vector<int> >(N, vector<int>(N, 0));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> mat[i][j];
+        }
+    }
+    mat2 = cal(mat, mat);
+    solve(B, result);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << result[i][j] % 1000 << ' ';
+        }
+        cout << '\n';
+    }
+}
