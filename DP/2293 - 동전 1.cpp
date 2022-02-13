6d058bcd698988f27aa12c613cd20cf54bff7403
Your Name <hwankim123@naver.com>
2022-02-13 17:45:45 +0900

    .

--- DP/2293 - 동전 1.cpp
+++ DP/2293 - 동전 1.cpp
@@ -1,56 +1,31 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-#define MAXN 101
 #define MAXK 10001
+#define MAXN 100
 
 int N, K;
-int cache[100001];
+int cache[MAXK];
 int coins[MAXN];
-
-int solve(int exchange, int coinIdx)
-{
-    // cout << exchange << ' ' << coinIdx << endl;
-    if (exchange < 0)
-    {
-        return 0;
-    }
-    if (exchange == 0)
-    {
-        return 1;
-    }
-    if (exchange != 0 && coinIdx == N - 1)
-    {
-        if (exchange % coins[coinIdx] == 0)
-            return 1;
-    }
-    if (cache[exchange] != 0)
-    {
-        return cache[exchange];
-    }
-    // 이부분고치면될듯
-    int sum = 0;
-    for (int i = coinIdx; i < N; i++)
-    {
-        if (exchange - coins[i] >= 0)
-        {
-            sum += solve(exchange - coins[i], i);
-        }
-    }
-    // cout << "cache updated " << "exchange : " << exchange << ' ' << sum << endl;
-    return cache[exchange] += sum;
-}
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
     cin >> N >> K;
     for (int i = 0; i < N; i++)
     {
         cin >> coins[i];
     }
-    sort(coins, coins + MAXN, greater<>());
-    cout << solve(K, 0);
+    cache[0] = 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = coins[i]; j <= K; j++)
+        {
+            if (j >= coins[i])
+            {
+                cache[j] += cache[j - coins[i]];
+            }
+        }
+    }
+    cout << cache[K];
 }
