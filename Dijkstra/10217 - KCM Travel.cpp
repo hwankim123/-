d0e740a05da563a626cf4f8bf213da51bce6790d
Your Name <hwankim123@naver.com>
2022-03-18 19:57:32 +0900

    .

---
+++ Dijkstra/10217 - KCM Travel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+#define MAXN 101
+#define INF 987654321
+
+int T;
+int N, M, K;
+vector<pair<int, int>> adj[MAXN];
+
+int main()
+{
+    cin >> T;
+    for (int tc = 0; tc < T; tc++)
+    {
+        cin >> N >> M >> K;
+        for (int i = 0; i < K; i++)
+        {
+            int u, v, c, d;
+            cin >> u >> v >> c >> d;
+        }
+    }
+}
