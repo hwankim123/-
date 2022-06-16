ebc605b6bfc9b8aadc73218ad5caf9df65ce7dbc
hwankim123 <hwankim1941@gmail.com>
2022-06-16 16:04:49 +0900

    revert 9546546db5c6dc8dc006cbdacf909867c2701ee5

--- Index Tree/2042 - 구간 합 구하기.cpp
+++ Index Tree/2042 - 구간 합 구하기.cpp
@@ -43,11 +43,11 @@ int main()
     cin >> N >> M >> K;
     for (B = 1; B < N; B <<= 1)
         ;
-    for (int i = B; i < N + B; i++)
+    for (int i = B; i < N + B; i++) // 데이터가 들어가는 시작 인덱스는 [2^k]
     {
         cin >> arr[i];
     }
-    for (int i = B - 1; i > 0; i--)
+    for (int i = B - 1; i > 0; i--) // 트리의 인덱스는 [1] ~ [2^k - 1]
     {
         arr[i] = arr[i * 2] + arr[i * 2 + 1];
     }
